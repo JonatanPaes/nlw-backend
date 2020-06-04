ca6667c9242b491e22370279cd39a4841c31077a
Jonatan Paes <jonatan.paes@itst.com.br>
2020-06-03 23:53:38 -0300

    criando metodos metodo

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,32 @@
-import express from "express";
+import express, { response } from 'express';
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  console.log("Listagem de Usuario");
+const users = [ 'Juninho', 'Fredão', 'Fred', 'Jonatan' ];
 
-  response.json(["Juninho", "Fred", "Jonatan"]);
+app.get('/users', (request, response) => {
+	const search = String(request.query.search);
+
+	const filtereUsers = search ? users.filter((user) => user.includes(search)) : users;
+
+	response.json(filtereUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+	const id = Number(request.params.id);
+
+	const user = users[id];
+
+	return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+	const user = {
+		name: 'Juninho',
+		email: 'juninho.paes.182@gmail.com'
+	};
+
+	return response.json(user);
 });
 
 app.listen(3333);
